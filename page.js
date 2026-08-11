edeea10ed354aa8b8416dcb68a971ebe64e315dc
yy292063-max <yy292063@gmail.com>
2026-08-11 01:29:52 -0700

    Update page.js

--- page.js
+++ page.js
@@ -1,5 +1,11 @@
 "use client";
+
 export default function Home() {
+
+  function testClick() {
+    alert("Button works!");
+  }
+
   return (
     <main
       style={{
@@ -10,8 +16,26 @@ export default function Home() {
       }}
     >
       <h1>IMTraderFX</h1>
+
       <h2>Mini App Test</h2>
+
       <p>Deployment Successful</p>
+
+      <button
+        onClick={testClick}
+        style={{
+          marginTop: "30px",
+          padding: "15px 30px",
+          background:"#2563eb",
+          color:"white",
+          border:"none",
+          borderRadius:"8px",
+          cursor:"pointer",
+        }}
+      >
+        TEST BUTTON
+      </button>
+
     </main>
   );
 }
