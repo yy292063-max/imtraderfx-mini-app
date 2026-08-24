b09f5d78d104ba01f6956f61d6597ffc45782b1b
yy292063-max <yy292063@gmail.com>
2026-08-24 00:40:29 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,15 +1,7 @@
 export default function Home() {
   return (
-    <main className="min-h-screen bg-slate-950 flex items-center justify-center">
-      <div className="rounded-2xl bg-blue-600 p-10 shadow-2xl">
-        <h1 className="text-5xl font-bold text-white">
-          IMTraderFX
-        </h1>
-
-        <p className="mt-4 text-blue-100">
-          Tailwind is working 🎉
-        </p>
-      </div>
-    </main>
+    <h1 style={{ color: "red", fontSize: "80px" }}>
+      TEST 123456
+    </h1>
   );
 }
